6b597a54acb569560910f080dc99c23de3932bff
Prarup Gurung <grgprarup@gmail.com>
2024-09-26 16:06:58 +0545

    Used reusable form field and separated components for signup screen

--- lib/src/screens/register_screen.dart
+++ lib/src/screens/register_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:guffgaff/src/screens/login_screen.dart';
 
+import '../widgets/custom_form_field.dart';
+
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
 
@@ -15,157 +17,160 @@ class _RegisterScreenState extends State<RegisterScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 15.0,
-            vertical: 20.0,
+      body: _buildUI(),
+    );
+  }
+
+  Widget _buildUI() {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 15.0,
+          vertical: 20.0,
+        ),
+        child: Column(
+          children: [
+            _headerText(),
+            _signUpForm(),
+            _loginLink(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _headerText() {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Create an Account",
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.w800,
+            ),
           ),
-          child: Column(
-            children: [
-              SizedBox(
-                width: MediaQuery.sizeOf(context).width,
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Create an Account",
-                      style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.w800,
-                      ),
-                    ),
-                    Text(
-                      "It's quick and easy",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                        color: Theme.of(context).colorScheme.secondary,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                height: MediaQuery.sizeOf(context).height * 0.70,
-                margin: EdgeInsets.symmetric(
-                  vertical: MediaQuery.sizeOf(context).height * 0.05,
-                ),
-                child: Form(
-                  key: _registerFormKey,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          // TODO: Select image
-                        },
-                        child: CircleAvatar(
-                          radius: MediaQuery.of(context).size.width * 0.15,
-                        ),
-                      ),
-                      SizedBox(
-                        height: MediaQuery.sizeOf(context).height * 0.1,
-                        child: TextFormField(
-                          onSaved: (value) {
-                            // TODO: Save the name
-                          },
-                          decoration: InputDecoration(
-                            labelText: "Full Name",
-                            border: const OutlineInputBorder(),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: MediaQuery.sizeOf(context).height * 0.1,
-                        child: TextFormField(
-                          onSaved: (value) {
-                            // TODO: Save the email
-                          },
-                          decoration: InputDecoration(
-                            labelText: "Email",
-                            border: const OutlineInputBorder(),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: MediaQuery.sizeOf(context).height * 0.1,
-                        child: TextFormField(
-                          onSaved: (value) {
-                            // TODO: Save the password
-                          },
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            labelText: "Password",
-                            border: const OutlineInputBorder(),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: MediaQuery.sizeOf(context).height * 0.1,
-                        child: TextFormField(
-                          onSaved: (value) {
-                            // TODO: Save the confirm password
-                          },
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            labelText: "Confirm Password",
-                            border: const OutlineInputBorder(),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: MediaQuery.sizeOf(context).width,
-                        child: MaterialButton(
-                          onPressed: () {
-                            // TODO: Validate the form
-                          },
-                          color: Theme.of(context).colorScheme.primary,
-                          child: const Text(
-                            "SIGN UP",
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text("Already have an account? "),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => LoginScreen()),
-                        );
-                      },
-                      child: Text(
-                        "Login",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w800,
-                          color: Theme.of(context).colorScheme.primary,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+          Text(
+            "It's quick and easy",
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _signUpForm() {
+    return Container(
+      height: MediaQuery.sizeOf(context).height * 0.70,
+      margin: EdgeInsets.symmetric(
+        vertical: MediaQuery.sizeOf(context).height * 0.05,
+      ),
+      child: Form(
+        key: _registerFormKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _profPicSelectionField(),
+            CustomFormField(
+              height: MediaQuery.sizeOf(context).height * 0.1,
+              labelText: "Full Name",
+              onSaved: (value) {
+                // TODO: Save the Full Name
+              },
+            ),
+            CustomFormField(
+              height: MediaQuery.sizeOf(context).height * 0.1,
+              labelText: "Email",
+              onSaved: (value) {
+                // TODO: Save the email
+              },
+            ),
+            CustomFormField(
+              height: MediaQuery.sizeOf(context).height * 0.1,
+              labelText: "Password",
+              onSaved: (value) {
+                // TODO: Save the password
+              },
+              obscureText: true,
+            ),
+            CustomFormField(
+              height: MediaQuery.sizeOf(context).height * 0.1,
+              labelText: "Confirm Password",
+              onSaved: (value) {
+                // TODO: Save the confirm password
+              },
+              obscureText: true,
+            ),
+            _signUpButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _profPicSelectionField() {
+    return GestureDetector(
+      onTap: () {
+        // TODO: Select profile picture
+      },
+      child: CircleAvatar(
+        radius: MediaQuery.of(context).size.width * 0.15,
+      ),
+    );
+  }
+
+  Widget _signUpButton() {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      child: MaterialButton(
+        onPressed: () {
+          // TODO: Validate the form
+        },
+        color: Theme.of(context).colorScheme.primary,
+        child: const Text(
+          "SIGN UP",
+          style: TextStyle(
+            color: Colors.white,
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _loginLink() {
+    return Expanded(
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text("Already have an account? "),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => LoginScreen()),
+              );
+            },
+            child: Text(
+              "Login",
+              style: TextStyle(
+                fontWeight: FontWeight.w800,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
